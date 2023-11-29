8ec80a843862aec513efe9da62c28ebbbf4c87fe
Sergienko Alexandr <alexander.s.sergienko@gmail.com>
2023-11-29 20:25:24 +0700

    сделана финкция заметок

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import telebot
 from telebot import types  # для указание типов
 import requests
+import config
 import numpy as np  #импорт библиотек для работы с exlel файлов
 import pandas as pd
-
-#token [|:0)
-bot = telebot.TeleBot('6498411034:AAEmtZA5dhLkHvpnJFhgq6lgVTffVmJQnUg')
+import threading
+import datetime
+bot = telebot.TeleBot('6840844685:AAG6vsQWmGVtpAVTnZFVyP8Y6op35FsxJ9k')
 
 
 @bot.message_handler(commands=['start'])
@@ -19,8 +20,6 @@ def start(message):
                      text="Привет, {0.first_name}! Я тестовый бот цифрик".format(
                          message.from_user) , reply_markup=markup)
 
-sch = pd.read_excel("C:\cifrick_tg_bot\расписание.xlsx")
-txt = str(sch.head())
 
 @bot.message_handler(content_types=['text'])
 def func(message):
@@ -50,13 +49,41 @@ def func(message):
             bot.send_message(message.from_user.id , '❌ На улице шторм, на улицу лучше не выходить')
 
     elif (message.text == "Расписание"):
-        bot.send_message(message.chat.id, txt)
+        bot.send_message(message.chat.id, "txt")
 
     elif (message.text == "Заметки"):
-        bot.send_message(message.chat.id , "У меня нет этой функции..")
+        bot.send_message(message.chat.id, 'Введите название заметки:')
+        bot.register_next_step_handler(message, set_reminder_name)
 
-    else:
-        bot.send_message(message.chat.id , text="На такую комманду я не запрограммирован...")
+def set_reminder_name(message):
+        user_data = {}
+        user_data[message.chat.id] = {'reminder_name': message.text}
+        bot.send_message(message.chat.id,
+                         'Введите дату и время, когда вы хотите получить напоминание в формате ГГГГ.ММ.ДД чч.мм')
+        bot.register_next_step_handler(message, reminder_set, user_data)
 
 
+def reminder_set(message, user_data):
+        try:
+            reminder_time = datetime.datetime.strptime(message.text, '%Y.%m.%d %H.%M')
+            now = datetime.datetime.now()
+            delta = reminder_time - now
+            if delta.total_seconds() <= 0:
+                bot.send_message(message.chat.id, 'Вы ввели прошедшую дату, попробуйте еще раз.')
+            else:
+                reminder_name = user_data[message.chat.id]['reminder_name']
+                bot.send_message(message.chat.id,
+                                 'Напоминание "{}" установлено на {}.'.format(reminder_name, reminder_time))
+                reminder_timer = threading.Timer(delta.total_seconds(), send_reminder, [message.chat.id, reminder_name])
+                reminder_timer.start()
+        except ValueError:
+            bot.send_message(message.chat.id, 'Вы ввели неверный формат даты и времени, попробуйте еще раз.')
+
+
+def send_reminder(chat_id, reminder_name):
+        bot.send_message(chat_id, 'Напоминание: "{}"'.format(reminder_name))
+
+
+
+#if __name__ == '__main__':
 bot.polling(none_stop=True)
